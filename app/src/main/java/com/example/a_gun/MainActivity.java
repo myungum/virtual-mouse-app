c90e1f6eeef0ac31401dae6d81e58616be5d2525
Yoo Myeongeum <gmyungum@gmail.com>
2022-04-22 23:09:57 +0900

    센서 관련 기능 분리 (Tracker.java)

--- app/src/main/java/com/example/a_gun/MainActivity.java
+++ app/src/main/java/com/example/a_gun/MainActivity.java
@@ -3,43 +3,22 @@ package com.example.a_gun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
-    public final int FLAG_LEFT = 0x01;
-    public final int FLAG_RIGHT = 0x02;
-    public final int FLAG_W = 0x04;
-    public final int FLAG_S = 0x08;
-    public final int FLAG_TAB = 0x10;
-    public final int FLAG_HOLD = 0x20;
-    private int mFlag = 0x00;
-
-    private Object mSensorValueLock = new Object();
     private Button mButtonLeft, mButtonRight, mButtonW, mButtonS, mButtonTab, mButtonHold;
-    private float mGyroX, mGyroY, mGyroZ;
-    private float mGravityX, mGravityZ;
     private final double WEIGHT = 70.0f; // mouse sensitivity
-    private SensorManager mSensorManager;
-    private Sensor mGyroSensor;
-    private Sensor mGravitySensor;
-    private UdpClient client;
+
+    private UdpClient mClient;
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,121 +31,70 @@ public class MainActivity extends AppCompatActivity {
         mButtonS = findViewById(R.id.btn_S);
         mButtonTab = findViewById(R.id.btn_Tab);
         mButtonHold = findViewById(R.id.btn_hold);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
-        mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        mClient = new UdpClient();
+        mClient.resumeServerFinder(); // start finding server in LAN
+        mClient.setOnFindServerListener(serverIP -> runOnUiThread(() -> {
+            Toast.makeText(getApplicationContext(), "server`s ip : " + serverIP, Toast.LENGTH_SHORT).show();
+        }));
+
+        mTracker = new Tracker((SensorManager)getSystemService(Context.SENSOR_SERVICE));
+        // if mouse pointer location is changed, send packet.
+        mTracker.setOnLocationChangeListener((x, y, flag) -> {
+            ByteBuffer buf = ByteBuffer.allocate(2 * Integer.BYTES + 1);
+            buf.putInt(x);
+            buf.putInt(y);
+            buf.put(flag);
+            mClient.send(buf.array());
+        });
 
         // button touch listener (make flag)
         View.OnTouchListener onTouchListener = (v, event) -> {
-            int currentFlag = (int)v.getTag();
-            synchronized (mSensorValueLock) {
-                switch (event.getAction() ) {
-                    case MotionEvent.ACTION_DOWN:
-                        mFlag |= currentFlag;
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        mFlag &= ~currentFlag;
-                        break;
-                }
+            Tracker.Flag currentFlag = (Tracker.Flag)v.getTag();
+            switch (event.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    mTracker.addFlag(currentFlag);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mTracker.removeFlag(currentFlag);
+                    break;
             }
-            make_and_send_data();
             return true;
         };
         // left button up/down event
-        mButtonLeft.setTag(FLAG_LEFT);
+        mButtonLeft.setTag(Tracker.Flag.LEFT);
         mButtonLeft.setOnTouchListener(onTouchListener);
 
         // right button up/down event
-        mButtonRight.setTag(FLAG_RIGHT);
+        mButtonRight.setTag(Tracker.Flag.RIGHT);
         mButtonRight.setOnTouchListener(onTouchListener);
 
         // w button up/down event
-        mButtonW.setTag(FLAG_W);
+        mButtonW.setTag(Tracker.Flag.W);
         mButtonW.setOnTouchListener(onTouchListener);
 
         // s button up/down event
-        mButtonS.setTag(FLAG_S);
+        mButtonS.setTag(Tracker.Flag.S);
         mButtonS.setOnTouchListener(onTouchListener);
 
-        // f button up/down event
-        mButtonTab.setTag(FLAG_TAB);
+        // tab button up/down event
+        mButtonTab.setTag(Tracker.Flag.TAB);
         mButtonTab.setOnTouchListener(onTouchListener);
 
         // hold button up/down event
-        mButtonHold.setTag(FLAG_HOLD);
+        mButtonHold.setTag(Tracker.Flag.HOLD);
         mButtonHold.setOnTouchListener(onTouchListener);
-
-
-        client = new UdpClient();
-        client.resumeServerFinder(); // start finding server in LAN
-        client.setOnFindServerListener(serverIP -> runOnUiThread(() -> {
-            Toast.makeText(getApplicationContext(), "server`s ip : " + serverIP, Toast.LENGTH_SHORT).show();
-        }));
     }
 
     public void onResume() {
         super.onResume();
-        mSensorManager.registerListener(gyroListener, mGyroSensor, SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(gravityListener, mGravitySensor, SensorManager.SENSOR_DELAY_GAME);
+        mTracker.resumeSensor();
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     public void onStop() {
         super.onStop();
-        mSensorManager.unregisterListener(gyroListener);
-        mSensorManager.unregisterListener(gravityListener);
+        mTracker.stopSensor();
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
-
-    // make mouse status data and send
-    private void make_and_send_data() {
-        double moveX, moveY ; // y+ is bottom, because left-top of screen is (0, 0)
-
-        synchronized (mSensorValueLock) {
-            final double scar = Math.sqrt(mGravityX * mGravityX + mGravityZ * mGravityZ); // gravity vector size on XZ-plane
-            final double s = mGravityX / scar; // sin
-            final double c = mGravityZ / scar; // cos
-
-            moveX = -mGyroZ * c - mGyroX * s; // x+ is right
-            moveY = mGyroZ * s - mGyroX * c; // y+ is bottom, because left-top of screen is (0, 0)
-        }
-
-        final int x = (int)(WEIGHT * moveX);
-        final int y = (int)(WEIGHT * moveY);
-        if (Math.abs(x) + Math.abs(y) >= 0) {
-            ByteBuffer buf = ByteBuffer.allocate(2 * Integer.BYTES + 1);
-            buf.putInt(x);
-            buf.putInt(y);
-            buf.put((byte)mFlag);
-            client.send(buf.array());
-        }
-    }
-
-    // gyro sensor
-    public SensorEventListener gyroListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int acc) {}
-        public void onSensorChanged(SensorEvent event) {
-            // calculation formula source : https://hackmd.io/oaTqnj61RCasSOSzaFw3Yw?view, https://www.youtube.com/watch?v=cRP3xnpOsM0
-            // but, this code use gravity instead of euler angle
-
-            synchronized (mSensorValueLock) {
-                mGyroX = event.values[0];
-                mGyroY = event.values[1];
-                mGyroZ = event.values[2];
-            }
-            make_and_send_data();
-        }
-    };
-    // gravity sensor
-    public SensorEventListener gravityListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int acc) {}
-        public void onSensorChanged(SensorEvent event) {
-            // float y = event.values[1]; consider only XZ-plane
-            synchronized (mSensorValueLock) {
-                mGravityX = event.values[0];
-                mGravityZ = event.values[2];
-            }
-            make_and_send_data();
-        }
-    };
 }
---
+++ app/src/main/java/com/example/a_gun/Tracker.java
@@ -0,0 +1,120 @@
+package com.example.a_gun;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class Tracker {
+    interface OnLocationChangeListener {
+        void onLocationChanged(int x, int y, byte flag);
+    }
+
+    public enum Flag {
+        LEFT(0x01),
+        RIGHT(0x02),
+        W(0x04),
+        S(0x08),
+        TAB(0x10),
+        HOLD(0x20);
+
+        private final int value;
+        Flag(int value) { this.value = value; }
+        public int getValue() { return value; }
+    }
+    private int mFlag = 0x00;
+
+    private Object mSensorValueLock = new Object();
+    private float mGyroX, mGyroY, mGyroZ;
+    private float mGravityX, mGravityZ;
+    private final double WEIGHT = 70.0f; // mouse sensitivity
+    private SensorManager mSensorManager;
+    private Sensor mGyroSensor;
+    private Sensor mGravitySensor;
+    private OnLocationChangeListener onLocationChangeListener;
+
+    public Tracker(SensorManager sensorManager) {
+        mSensorManager = sensorManager;
+        mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED);
+        mGravitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        mSensorManager.registerListener(gyroListener, mGyroSensor, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(gravityListener, mGravitySensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    // make mouse status data
+    private void locationChange() {
+        double moveX, moveY ; // y+ is bottom, because left-top of screen is (0, 0)
+
+        synchronized (mSensorValueLock) {
+            final double scar = Math.sqrt(mGravityX * mGravityX + mGravityZ * mGravityZ); // gravity vector size on XZ-plane
+            final double s = mGravityX / scar; // sin
+            final double c = mGravityZ / scar; // cos
+
+            moveX = -mGyroZ * c - mGyroX * s; // x+ is right
+            moveY = mGyroZ * s - mGyroX * c; // y+ is bottom, because left-top of screen is (0, 0)
+        }
+
+        final int x = (int)(WEIGHT * moveX);
+        final int y = (int)(WEIGHT * moveY);
+
+        if (onLocationChangeListener != null)
+            onLocationChangeListener.onLocationChanged(x, y, (byte)mFlag);
+    }
+
+    public void setOnLocationChangeListener(OnLocationChangeListener onLocationChangeListener) {
+        this.onLocationChangeListener = onLocationChangeListener;
+    }
+
+    // gyro sensor
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) {}
+        public void onSensorChanged(SensorEvent event) {
+            // calculation formula source : https://hackmd.io/oaTqnj61RCasSOSzaFw3Yw?view, https://www.youtube.com/watch?v=cRP3xnpOsM0
+            // but, this code use gravity instead of euler angle
+
+            synchronized (mSensorValueLock) {
+                mGyroX = event.values[0];
+                mGyroY = event.values[1];
+                mGyroZ = event.values[2];
+            }
+            locationChange();
+        }
+    };
+    // gravity sensor
+    public SensorEventListener gravityListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) {}
+        public void onSensorChanged(SensorEvent event) {
+            // float y = event.values[1]; consider only XZ-plane
+            synchronized (mSensorValueLock) {
+                mGravityX = event.values[0];
+                mGravityZ = event.values[2];
+            }
+            locationChange();
+        }
+    };
+
+    public void stopSensor() {
+        mSensorManager.unregisterListener(gyroListener);
+        mSensorManager.unregisterListener(gravityListener);
+    }
+
+    public void resumeSensor() {
+        mSensorManager.registerListener(gyroListener, mGyroSensor, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(gravityListener, mGravitySensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    // add flag (mouse down, key down...)
+    public void addFlag(Flag flag) {
+        synchronized (mSensorValueLock) {
+            mFlag |= flag.getValue();
+        }
+    }
+
+    // remove flag (mouse up, key up...)
+    public void removeFlag(Flag flag) {
+        synchronized (mSensorValueLock) {
+            mFlag &= ~flag.getValue();
+        }
+    }
+}
